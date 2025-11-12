031a54b21cff6a258b8a2df5fd7b8ee859e3d1c2
DanielBFiel13 <danigrand3@gmail.com>
2025-11-12 01:00:55 +0000

    Actualizacion a relacionado con profesor 1.0

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -3,11 +3,11 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
-class Tarea extends Model
+class Task extends Model
 {   
-    use HasFactory;
+    use SoftDeletes;
 
     protected $fillable = [
         'titulo',
